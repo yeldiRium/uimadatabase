46374348c42c38a46503625f1c52a9c9aa321474
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-07 13:43:15 +0100

    update BaseX Connection test: reformat logging and add check whether connection already exists to loop

--- src/main/java/dbtest/MainContainerTest.java
+++ src/main/java/dbtest/MainContainerTest.java
@@ -100,18 +100,25 @@ public class MainContainerTest {
 	 * Tests the connection to the BaseX container.
 	 */
 	private class BaseXTest extends DBConnectionTest {
+		protected ClientSession session;
 		@Override
 		protected void tryToConnect() {
+			// If the connection is already established, nothing further has to be done
+			if(this.session != null) {
+				System.out.println("BaseX: Connected.");
+				return;
+			}
+			
 			try {
 				final String host = System.getenv("BASEX_HOST");
 				final int port = Integer.parseInt(System.getenv("BASEX_PORT"));
 				final String username = System.getenv("BASEX_USER");
 				final String password = System.getenv("BASEX_PASS");
-				ClientSession session = new ClientSession(host, port, username, password);
-				System.out.println("Connection to BaseXHTTP Server successful!");
-				Thread.currentThread().interrupt();
+				// Create client with credentials from environment variable
+				this.session = new ClientSession(host, port, username, password);
+				System.out.println("BaseX: Connection successful!");
 			} catch (IOException e) {
-				System.out.println("Connection to BaseXHTTP Server could not be established. Retrying...");
+				System.out.println("BaseX: Connection failed. Retrying...");
 			}
 		}
 	}
