7930b2abe9ce8e2a81a92d466e156e0d6835fe0d
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-07 14:09:13 +0100

    add successful cassandra connection test

--- src/main/java/dbtest/MainContainerTest.java
+++ src/main/java/dbtest/MainContainerTest.java
@@ -10,6 +10,10 @@ import org.basex.api.client.ClientSession;
 import com.arangodb.ArangoDB;
 import com.arangodb.ArangoDBException;
 import com.arangodb.entity.ArangoDBVersion;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.AuthenticationException;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
 
 /**
  * This tests the connection to all configured databases.
@@ -127,9 +131,29 @@ public class MainContainerTest {
 	 * Tests the connection to the Cassandra container.
 	 */
 	private class CassandraTest extends DBConnectionTest {
+		protected Cluster cluster;
+		protected Session session;
 		@Override
 		protected void tryToConnect() {
-			System.out.println("CassandraTest running...");
+			// If the connection is already established, nothing further has to be done
+			if(this.session != null) {
+				System.out.println("Cassandra: connected.");
+				return;
+			}
+			
+			try {
+				// Create cluster with credentials from environment variables
+				this.cluster = Cluster.builder()
+					.addContactPoint(System.getenv("CASSANDRA_HOST"))
+					.withPort(Integer.parseInt(System.getenv("CASSANDRA_PORT")))
+					.withCredentials(System.getenv("CASSANDRA_USER"), System.getenv("CASSANDRA_PASS"))
+					.build();
+				// Connect to server
+				this.session = cluster.connect();
+				System.out.println("Cassandra: Connection successful!");
+			} catch(NoHostAvailableException|AuthenticationException|IllegalStateException e) {
+				System.out.println("Cassandra: Connection failed. Retrying...");
+			}
 		}
 	}
 	
