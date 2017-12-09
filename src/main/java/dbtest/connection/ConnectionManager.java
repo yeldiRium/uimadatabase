88f2010e5ce7f6a64cac12d2f0bca2a2e08f4d09
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-09 22:44:12 +0100

    rewrite ConnectionManager to work with Future objects

--- src/main/java/dbtest/connection/ConnectionManager.java
+++ src/main/java/dbtest/connection/ConnectionManager.java
@@ -3,6 +3,11 @@ package dbtest.connection;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,134 +15,66 @@ import dbtest.connection.exception.ConnectionRequestAlreadySubmittedException;
 
 public class ConnectionManager {
 	protected static final Logger LOGGER = Logger.getLogger(ConnectionManager.class.getName());
+	protected static final int threadCount = 10;
 	
-	/**
-	 * How long the Thread should wait between checking the connections.
-	 */
-	protected final int sleepTime = 200;
-	protected Map<ConnectionRequest, ConnectionResponse> connectionRequests;
-	protected Map<Class<?extends Connection>, Connection> connections;
-	protected Thread checkThread;
-	
-	protected class ConnectionChecker implements Runnable {
-		protected boolean interrupted = false;
-		
-		/**
-		 * Regularly processes all current requests.
-		 * If there are no more requests afterwards, the Thread is stopped.
-		 */
-		@Override
-		public void run() {
-			while (!Thread.currentThread().isInterrupted()) {
-				try {
-					LOGGER.fine("Starting connection check.");
-					for(ConnectionRequest connectionRequest: connectionRequests.keySet()) {
-						processRequest(connectionRequest);
-					}
-					// If there are no requests pending, stop the watching thread.
-					// It will be restarted once new requests come in.
-					if(connectionRequests.size() == 0) {
-						LOGGER.fine("No Request in queue, stopping ConnectionChecker.");
-						throw new InterruptedException();
-					}
-					Thread.sleep(sleepTime);
-				} catch (InterruptedException ex) {
-					LOGGER.fine("ConnectionChecker stopped.");
-					Thread.currentThread().interrupt();
-					this.interrupted = true;
-				}
-			}
-		}
-	}
+	protected Map<Class<?extends Connection>, Future<Connection>> connections;
+	protected ExecutorService executor;
 	
 	public ConnectionManager() {
-		this.connectionRequests = new HashMap<>();
 		this.connections = new HashMap<>();
+		this.executor = Executors.newFixedThreadPool(ConnectionManager.threadCount);
 		LOGGER.info("ConnectionManager initialized.");
-	}
-	
-	/**
-	 * Starts a watching thread that checks connections for their status.
-	 * Will be stopped as soon as there are no more requests to process.
-	 */
-	public void watchConnections() {
-		if((this.checkThread == null) || (!this.checkThread.isAlive())) {
-			ConnectionChecker checker = new ConnectionChecker();
-			this.checkThread = new Thread(checker);
-			this.checkThread.start();
-			LOGGER.fine("ConnectionChecker started.");
-		}
 	}
 
 	/**
-	 * Adds a request for processing.
-	 * Creates an empty Response object.
-	 * 
-	 * Starts the watching thread, if there is none running currently.
-	 * @param connectionRequest
-	 */
-	public void submitRequest(ConnectionRequest connectionRequest) {
-		LOGGER.finer("Request received. Endpoint is " + connectionRequest.getResponseEndpoint().getClass().getName());
-		if(this.connectionRequests.get(connectionRequest) != null) {
-			LOGGER.finer("Request already in queue. Aborting submission...");
-			throw new ConnectionRequestAlreadySubmittedException();
-		}
-		ConnectionResponse connectionResponse = new ConnectionResponse();
-		
-		this.connectionRequests.put(connectionRequest, connectionResponse);
-		this.processRequest(connectionRequest);
-		
-		// Watch connections after first processing.
-		this.watchConnections();
-	}
-	
-	/**
 	 * Checks all connections the given Request wants.
-	 * If a connection is not registered yet, it will be created and started to establish.
-	 * If a connection is already established, it will be added to the according response object.
+	 * If a connection is not registered yet, a future for it will be created.
+	 * If a connection is already established, it will be added to a response object.
 	 * If all connections are established, the Response is finished and the responseEndpoint called.
 	 * @param connectionRequest
 	 */
-	protected void processRequest(ConnectionRequest connectionRequest) {
-		LOGGER.finer("Processing request...");
-		ConnectionResponse connectionResponse = this.connectionRequests.get(connectionRequest);
-		// Will be set to false by any not yet established connection.
-		boolean isRequestComplete = true;
-		for(Class<?extends Connection> cls: connectionRequest.getRequestedConnections()) {
-			Connection connection = this.connections.get(cls);
-			if(connection == null) {
-				try {
-					connection = cls.getConstructor().newInstance();
-				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-						| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-					// will never happen, since Connection has a fitting Constructor and all classes used
-					// here extend Connection.
-					e.printStackTrace();
-				}
-				this.connections.put(cls, connection);
+	public Future<ConnectionResponse> submitRequest(ConnectionRequest connectionRequest) {
+		LOGGER.finer("Request received. Processing in new Thread...");
+		return this.executor.submit(new Callable<ConnectionResponse>() {
+			@Override
+			public ConnectionResponse call() throws InterruptedException, ExecutionException {
+				LOGGER.finer("Processing Thread started.");
+				ConnectionResponse connectionResponse = new ConnectionResponse();
 				
-				// This starts a thread that tries to connect several times.
-				// There will be no immediate effect.
-				connection.establish();
-				LOGGER.info("Started connection to " + cls.getName());
+				for(Class<?extends Connection> cls: connectionRequest.getRequestedConnections()) {
+					Future<Connection> connectionFuture = connections.get(cls);
+					if(connectionFuture == null) {
+						LOGGER.finer("Connection to " + cls.getName() + " not found. Starting one in new Thread...");
+						connectionFuture = executor.submit(new Callable<Connection>() {
+							public Connection call() {
+								LOGGER.finer("Connection Thread started.");
+								try {
+									Connection connection = cls.getConstructor().newInstance();
+									connection.establish();
+									return connection;
+								} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+										| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+									// will never happen, since Connection has a fitting Constructor and all classes used
+									// here extend Connection.
+									LOGGER.severe("Exception occurred during establishment of Connection to " + cls.getName());
+									e.printStackTrace();
+									return null;
+								}
+							}
+						});
+						connections.put(cls, connectionFuture);
+					}
+				}
+				// get the Futures' responses in a second loop, so all of them are started before
+				// the first blocking execution.
+				for(Class<?extends Connection> cls: connectionRequest.getRequestedConnections()) {
+					Future<Connection> connectionFuture = connections.get(cls);
+					connectionResponse.addConnection(connectionFuture.get());
+				}
+				// Call endpoint with Response object.
+				LOGGER.info("Response built. Returning...");
+				return connectionResponse;
 			}
-			// Will usually not be true, if the above block was executed.
-			// There is no harm in testing it immediately, though.
-			if(connection.isEstablished()) {
-				connectionResponse.addConnection(connection);
-				isRequestComplete &= true;
-				LOGGER.fine("Connection to " + connection.getClass().getName() + " is open.");
-				continue;
-			}
-			LOGGER.fine("Connection to " + connection.getClass().getName() + " is not yet open.");
-			isRequestComplete &= false;
-		}
-		if(isRequestComplete) {
-			// Call endpoint with Response object.
-			LOGGER.info("Request finished. Sending response to " + connectionRequest.getResponseEndpoint().getClass().getName());
-			connectionResponse.finish();
-			connectionRequest.getResponseEndpoint().acceptResponse(connectionResponse);
-			this.connectionRequests.remove(connectionRequest);
-		}
+		});
 	}
 }
--- src/test/java/uimadatabase/dbtest/connection/ConnectionManagerTestCase.java
+++ src/test/java/uimadatabase/dbtest/connection/ConnectionManagerTestCase.java
@@ -1,21 +1,20 @@
 package uimadatabase.dbtest.connection;
 
 import java.util.Set;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.verify;
 
 import dbtest.connection.AcceptsConnectionResponse;
 import dbtest.connection.Connection;
 import dbtest.connection.ConnectionManager;
 import dbtest.connection.ConnectionRequest;
 import dbtest.connection.ConnectionResponse;
-import dbtest.connection.exception.ConnectionRequestAlreadySubmittedException;
 
 class ConnectionManagerTestCase {
 	/**
@@ -23,21 +22,17 @@ class ConnectionManagerTestCase {
 	 */
 	public static class MockConnection extends Connection {
 		public int establishCounter = 0;
-		public int isEstablishedCounter = 0;
 		
 		public MockConnection() {
 			
 		}
 		
 		public void establish() {
-			this.establishCounter++;;
+			this.establishCounter++;
 		}
 		
 		public boolean isEstablished() {
-			this.isEstablishedCounter++;
-			latch.countDown();
-			// Let's ask 5 times, then returns true.
-			return this.isEstablishedCounter >= 5;
+			return this.establishCounter >= 1;
 		}
 
 		@Override
@@ -61,46 +56,22 @@ class ConnectionManagerTestCase {
 	
 	protected ArgumentCaptor<ConnectionResponse> captor;
 	
-	protected static CountDownLatch latch;
-	
 	@Tag("slow")
 	@Test
-	void Given_ConnectionManagerAndMockedConnection_When_SubmittingRequestForMockedConnection_Then_EstablishAndIsEstablishedWillBeCalledOnMock() {
-		ConnectionManagerTestCase.latch = new CountDownLatch(5);
+	void Given_ConnectionManagerAndMockedConnection_When_SubmittingRequestForMockedConnection_Then_EstablishAndIsEstablishedWillBeCalledOnMock() throws InterruptedException, ExecutionException {
 		AcceptsConnectionResponse mockedResponseEndpoint = Mockito.mock(AcceptsConnectionResponse.class);
 		ConnectionManager connectionManager = new ConnectionManager();
 		ConnectionRequest connectionRequest = new ConnectionRequest(mockedResponseEndpoint);
 		connectionRequest.addRequestedConnection(MockConnection.class);
 		
-		connectionManager.submitRequest(connectionRequest);
-		try {
-			latch.await();
-		} catch (InterruptedException e) {
-			// IDGAF
-		}
-		
-		// Test, that the mockedResponseEndpoint actually got a response
-		this.captor = ArgumentCaptor.forClass(ConnectionResponse.class);
-		verify(mockedResponseEndpoint).acceptResponse(this.captor.capture());
-		ConnectionResponse connectionResponse = captor.getValue();
+		Future<ConnectionResponse> futureConnectionResponse = connectionManager.submitRequest(connectionRequest);
+
+		ConnectionResponse connectionResponse = futureConnectionResponse.get();
 		Set<Connection> connections = connectionResponse.getConnections();
 		
 		assertEquals(1, connections.size(), "One Connection was requested, one should be returned.");
 		MockConnection mockedConnection = (MockConnection) connections.toArray()[0];
 		assertEquals(1, mockedConnection.establishCounter, "Establish should've been called exactly once.");
-		assertEquals(5, mockedConnection.isEstablishedCounter, "IsEstablished should've been called exactly five times.");
-	}
-	
-	@Test
-	void Given_ConnectionManager_When_SubmittingConnectionMultipleTimes_Then_ExceptionIsThrown() {
-		AcceptsConnectionResponse mockedResponseEndpoint = Mockito.mock(AcceptsConnectionResponse.class);
-		ConnectionManager connectionManager = new ConnectionManager();
-		ConnectionRequest connectionRequest = new ConnectionRequest(mockedResponseEndpoint);
-		connectionRequest.addRequestedConnection(NoConnection.class);
-		connectionManager.submitRequest(connectionRequest);
-		assertThrows(
-			ConnectionRequestAlreadySubmittedException.class,
-			() -> connectionManager.submitRequest(connectionRequest)
-		);
+		assertEquals(true, mockedConnection.isEstablished(), "IsEstablished should be true by now.");
 	}
 }
