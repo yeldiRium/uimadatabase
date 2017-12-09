c17db5127f0fa99d0f84b675d42225bd31f6e709
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-09 22:57:16 +0100

    adjust DBConnectionTest to changes in connection package

--- src/main/java/dbtest/DBConnectionTest.java
+++ src/main/java/dbtest/DBConnectionTest.java
@@ -1,6 +1,9 @@
 package dbtest;
 
-import dbtest.connection.AcceptsConnectionResponse;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import dbtest.connection.Connection;
 import dbtest.connection.ConnectionManager;
 import dbtest.connection.ConnectionRequest;
 import dbtest.connection.ConnectionResponse;
@@ -18,22 +21,33 @@ import dbtest.connection.implementation.Neo4jConnection;
  * 
  * @author Hannes Leutloff <hannes.leutloff@aol.de>
  */
-public class DBConnectionTest implements AcceptsConnectionResponse {
+public class DBConnectionTest {
 	public static void main(String[] args) {
 		ConnectionManager connectionManager = new ConnectionManager();
-		DBConnectionTest main = new DBConnectionTest();
-		ConnectionRequest connectionRequest = new ConnectionRequest(main);
+		
+		ConnectionRequest connectionRequest = new ConnectionRequest();
 		connectionRequest.addRequestedConnection(ArangoDBConnection.class);
 		connectionRequest.addRequestedConnection(BaseXConnection.class);
 		connectionRequest.addRequestedConnection(CassandraConnection.class);
 		connectionRequest.addRequestedConnection(MongoDBConnection.class);
 		connectionRequest.addRequestedConnection(MySQLConnection.class);
 		connectionRequest.addRequestedConnection(Neo4jConnection.class);
-		connectionManager.submitRequest(connectionRequest);
-	}
-
-	@Override
-	public void acceptResponse(ConnectionResponse response) {
-		System.out.println("Response received. Connection to all databases established.");
+		
+		Future<ConnectionResponse> futureConnectionResponse
+			= connectionManager.submitRequest(connectionRequest);
+		
+		try {
+			ConnectionResponse connectionResponse = futureConnectionResponse.get();
+			System.out.println("Connections Established:");
+			for(Connection connection: connectionResponse.getConnections()) {
+				if(connection.isEstablished()) {					
+					System.out.println("Connection " + connection.getClass().getName() + " established.");
+				}
+			}
+			
+		} catch (InterruptedException | ExecutionException e) {
+			System.out.println("Something went really wrong:");
+			e.printStackTrace();
+		}
 	}
 }
