bae9ec9a01b8b9585643a81baf1150f157c29a14
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-18 18:00:40 +0100

    clean up and document CassandraCounterCreateEvaluationCase further

--- src/main/java/dbtest/evaluations/CassandraCounterCreateEvaluationCase.java
+++ src/main/java/dbtest/evaluations/CassandraCounterCreateEvaluationCase.java
@@ -11,6 +11,13 @@ import org.hucompute.services.uima.database.cassandra.CassandraIndexWriter;
 import java.io.*;
 import java.util.Set;
 
+/**
+ * Benchmarks operations on existing data.
+ * Assumes that content in tables 'lemma' and 'text_xmi' exists.
+ * Also several operations depend on other operation's results to be present in
+ * the database.
+ * Thus order should be preserved or dependencies analyzed when refactoring.
+ */
 public class CassandraCounterCreateEvaluationCase implements EvaluationCase
 {
 	@Override
@@ -32,18 +39,9 @@ public class CassandraCounterCreateEvaluationCase implements EvaluationCase
 			output = new PrintWriter(new FileOutputStream(new File("output/CassandraCounterCreateEvaluationCase.txt")));
 
 			Session session = cassandraConnection.getSession();
+
 			long start;
 			long end;
-
-			int lcCount = 0;
-			int dlcCount = 0;
-			int ttrWCount = 0;
-			int ttrRCount = 0;
-			int loCount = 0;
-			int tfCount = 0;
-			int idfCount = 0;
-			int tfidfCount = 0;
-			int tfidfRCount = 0;
 
 			session.execute("create keyspace if not exists textimager with replication = {'class': 'SimpleStrategy', 'replication_factor': 1};");
 			session.execute("use textimager;");
@@ -51,62 +49,51 @@ public class CassandraCounterCreateEvaluationCase implements EvaluationCase
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeLemmaCounter(session);
 			end = System.currentTimeMillis();
-			lcCount=(int) (end-start);
 			output.write("Creating lemmaCounter took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeDistinctLemmaCounter(session);
 			end = System.currentTimeMillis();
-			dlcCount=(int) (end-start);
 			output.write("Creating distinctLemmaCounter took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeTTR(session);
 			end = System.currentTimeMillis();
-			ttrWCount=(int) (end-start);
 			output.write("Creating TTR took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 //			HashMap<String, Float> ttr = CassandraIndexWriter.getTTR(session);
 //			output.write();(ttr);
 			end = System.currentTimeMillis();
-			ttrRCount=(int) (end-start);
 			output.write("Getting TTR took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeLemmaOccurrences(session);
 			end = System.currentTimeMillis();
-			loCount=(int) (end-start);
 			output.write("Writing lemmaOccurences took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeIDF(session);
 			end = System.currentTimeMillis();
-			idfCount=(int) (end-start);
 			output.write("Writing idf took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeFrequencyNormalizedByLength(session);
 			end = System.currentTimeMillis();
-			tfCount=(int) (end-start);
 			output.write("Writing tf took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 			CassandraIndexWriter.writeTfIdf(session);
 			end = System.currentTimeMillis();
-			tfidfCount=(int) (end-start);
 			output.write("Writing tf-idf took: "+(end-start)+"ms.");
 
 			start = System.currentTimeMillis();
 //			CassandraIndexWriter.getTFIDF(session);
 			end = System.currentTimeMillis();
-			tfidfRCount=(int) (end-start);
 			output.write("Reading tf-idf took: "+(end-start)+"ms.");
 
 			session.execute("drop keyspace textimager;");
 			output.write("!!!Dropped keyspace!!!");
-
-			output.write("Testing Times\nlc = "+lcCount+"\ndlc = "+dlcCount+"\nttrW = "+ttrWCount+"\nttrR = "+ttrRCount+"\nlo = "+loCount+"\nidf = "+idfCount+"\ntf = "+tfCount+"\ntd-idf = "+tfidfCount+"\nGet td-idf = "+tfidfRCount);
 		} catch (IOException e)
 		{
 			e.printStackTrace();
