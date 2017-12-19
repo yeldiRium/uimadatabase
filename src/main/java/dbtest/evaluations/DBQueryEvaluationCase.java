3c9830ce51a169e0fc49b057be27b503cd3f457e
Hannes Leutloff <hannes.leutloff@aol.de>
2017-12-19 09:44:49 +0100

    convert DBQueryTest to EvaluationCase implementation

--- src/main/java/dbtest/evaluations/DBQueryEvaluationCase.java
+++ src/main/java/dbtest/evaluations/DBQueryEvaluationCase.java
@@ -1,12 +1,10 @@
-package dbtest;
+package dbtest.evaluations;
 
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
-import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-
+import com.google.common.collect.Iterators;
+import dbtest.connection.ConnectionRequest;
+import dbtest.connection.ConnectionResponse;
+import dbtest.evaluationFramework.EvaluationCase;
+import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
 import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.collection.CollectionReader;
@@ -21,24 +19,43 @@ import org.hucompute.services.uima.database.xmi.XmiReaderModified;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
-import org.xml.sax.SAXException;
 
-import com.google.common.collect.Iterators;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 
-import de.tudarmstadt.ukp.dkpro.core.io.xmi.XmiReader;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
 
-public class DBQueryTest {
-	public static void main(String...args) throws UIMAException, IOException, SAXException{
-
-		//		getPosXMI();
-//		   	getLemmaXMI();
-
-//			getPosDistributionNeo4j();
-//			getLemmaDistributionNeo4j();
-//			getTypeTokenDistributionNeo4j();as
+public class DBQueryEvaluationCase implements EvaluationCase
+{
+	@Override
+	public ConnectionRequest requestConnection()
+	{
+		// TODO: decide what to request here
+		return null;
 	}
 
-	public static void getPosXMI() throws UIMAException, IOException{
+	@Override
+	public void run(ConnectionResponse connectionResponse)
+	{
+
+		try
+		{
+			DBQueryEvaluationCase.getPosXMI();
+			DBQueryEvaluationCase.getLemmaXMI();
+
+			DBQueryEvaluationCase.getPosDistributionNeo4j();
+			DBQueryEvaluationCase.getLemmaDistributionNeo4j();
+			DBQueryEvaluationCase.getTypeTokenDistributionNeo4j();
+		} catch (UIMAException | IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void getPosXMI() throws UIMAException, IOException
+	{
 		long start = System.currentTimeMillis();
 		CollectionReader xmireader = CollectionReaderFactory.createReader(XmiReaderModified.class,
 				XmiReader.PARAM_SOURCE_LOCATION,"/media/ahemati/cea5347d-36d3-4856-a9be-bcd0bddbfd92/wikipedia_kategorien_sample/biologie",
@@ -53,7 +70,7 @@ public class DBQueryTest {
 		FileUtils.writeStringToFile(new File("dbtest/query/xmi_sum_pos.log"), ""+(System.currentTimeMillis()-start));
 		System.out.println(System.currentTimeMillis()-start);
 	}
-	
+
 	public static void getLemmaXMI() throws UIMAException, IOException{
 		long start = System.currentTimeMillis();
 		CollectionReader xmireader = CollectionReaderFactory.createReader(XmiReaderModified.class,
@@ -66,7 +83,7 @@ public class DBQueryTest {
 		runPipeline(
 				xmireader,
 				createEngine(POSCounter.class));
-		FileUtils.writeStringToFile(new File("dbtest/query/xmi_sum_lemma.log"), ""+(System.currentTimeMillis()-start));
+		FileUtils.writeStringToFile(new File("output/DBQueryEvaluationCase_xmiSumLemma.log"), ""+(System.currentTimeMillis()-start));
 		System.out.println(System.currentTimeMillis()-start);
 	}
 
@@ -82,10 +99,10 @@ public class DBQueryTest {
 				System.out.println(Iterators.size(n.getRelationships(Direction.INCOMING, Const.RelationType.pos).iterator()));
 			});
 		}
-		FileUtils.writeStringToFile(new File("dbtest/query/neo4j_sum_pos.log"), ""+(System.currentTimeMillis()-start));
+		FileUtils.writeStringToFile(new File("output/DBQueryEvaluationCase_neo4jSumPos.log"), ""+(System.currentTimeMillis()-start));
 		System.out.println(System.currentTimeMillis()-start);
 	}
-	
+
 	static int i = 0;
 	public static void getLemmaDistributionNeo4j() throws UIMAException, IOException{
 		long start = System.currentTimeMillis();
@@ -93,9 +110,9 @@ public class DBQueryTest {
 		MDB_Neo4J_Impl pMDB = new MDB_Neo4J_Impl("/home/ahemati/workspace/XMI4Neo4J/conf.conf");
 
 		Iterator<Node> pos = pMDB.getNodes(Lemma_Neo4J_Impl.getLabel()).iterator();
-		
+
 		try (Transaction tx = MDB_Neo4J_Impl.gdbs.beginTx()) {
-			
+
 			pos.forEachRemaining(n->{
 				if(i++%1000==0)
 					System.out.println(i);
@@ -103,19 +120,19 @@ public class DBQueryTest {
 			});
 			System.out.println(i);
 		}
-		FileUtils.writeStringToFile(new File("dbtest/query/neo4j_sum_lemma.log"), ""+(System.currentTimeMillis()-start));
+		FileUtils.writeStringToFile(new File("output/DBQueryEvaluationCase_neo4jSumLemma.log"), ""+(System.currentTimeMillis()-start));
 		System.out.println(System.currentTimeMillis()-start);
 	}
-	
+
 	public static void getLemmaDistributionBaseX() throws UIMAException, IOException{
 		long start = System.currentTimeMillis();
 
 		MDB_Neo4J_Impl pMDB = new MDB_Neo4J_Impl("/home/ahemati/workspace/XMI4Neo4J/conf.conf");
 
 		Iterator<Node> pos = pMDB.getNodes(Lemma_Neo4J_Impl.getLabel()).iterator();
-		
+
 		try (Transaction tx = MDB_Neo4J_Impl.gdbs.beginTx()) {
-			
+
 			pos.forEachRemaining(n->{
 				if(i++%1000==0)
 					System.out.println(i);
@@ -123,29 +140,29 @@ public class DBQueryTest {
 			});
 			System.out.println(i);
 		}
-		FileUtils.writeStringToFile(new File("dbtest/query/neo4j_sum_lemma.log"), ""+(System.currentTimeMillis()-start));
+		FileUtils.writeStringToFile(new File("output/DBQueryEvaluationCase_neo4jSumLemma.log"), ""+(System.currentTimeMillis()-start));
 		System.out.println(System.currentTimeMillis()-start);
 	}
-	
-//	public static void getTypeTokenDistributionNeo4j() throws UIMAException, IOException{
-//		long start = System.currentTimeMillis();
-//
-//		MDB_Neo4J_Impl pMDB = new MDB_Neo4J_Impl("/home/ahemati/workspace/XMI4Neo4J/conf.conf");
-//
-//		Iterator<Node> documents = pMDB.getNodes(Document_Neo4J_Impl.getLabel()).iterator();
-//		
-//		try (Transaction tx = MDB_Neo4J_Impl.gdbs.beginTx()) {
-//			
-//			documents.forEachRemaining(n->{
-//				
-////				System.out.println(n);
-////				if(i++%1000==0)
-////					System.out.println(i);
-////				Iterators.size(n.getRelationships(Direction.INCOMING, Const.RelationType.lemma).iterator());
-//			});
-//			System.out.println(i);
-//		}
-//		FileUtils.writeStringToFile(new File("dbtest/query/neo4j_type_token.log"), ""+(System.currentTimeMillis()-start));
-//		System.out.println(System.currentTimeMillis()-start);
-//	}
+
+	public static void getTypeTokenDistributionNeo4j() throws UIMAException, IOException{
+		long start = System.currentTimeMillis();
+
+		MDB_Neo4J_Impl pMDB = new MDB_Neo4J_Impl("/home/ahemati/workspace/XMI4Neo4J/conf.conf");
+
+		Iterator<Node> documents = pMDB.getNodes(Document_Neo4J_Impl.getLabel()).iterator();
+
+		try (Transaction tx = MDB_Neo4J_Impl.gdbs.beginTx()) {
+
+			documents.forEachRemaining(n->{
+
+//				System.out.println(n);
+//				if(i++%1000==0)
+//					System.out.println(i);
+//				Iterators.size(n.getRelationships(Direction.INCOMING, Const.RelationType.lemma).iterator());
+			});
+			System.out.println(i);
+		}
+		FileUtils.writeStringToFile(new File("output/DBQueryEvaluationCase_neo4jTypeToken.log"), ""+(System.currentTimeMillis()-start));
+		System.out.println(System.currentTimeMillis()-start);
+	}
 }
