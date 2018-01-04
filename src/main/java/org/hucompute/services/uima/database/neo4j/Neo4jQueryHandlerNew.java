c504a3664b942a364370301f3fdf0dd52f9babd6
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-04 14:28:34 +0100

    implement Neo4jQueryHandlerNew#storeJCasDocument

--- src/main/java/org/hucompute/services/uima/database/neo4j/Neo4jQueryHandlerNew.java
+++ src/main/java/org/hucompute/services/uima/database/neo4j/Neo4jQueryHandlerNew.java
@@ -1,5 +1,10 @@
 package org.hucompute.services.uima.database.neo4j;
 
+import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Paragraph;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.hucompute.services.uima.database.AbstractQueryHandler;
 import org.hucompute.services.uima.database.Const;
@@ -9,13 +14,17 @@ import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.StatementResult;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Neo4jQueryHandlerNew extends AbstractQueryHandler
 {
 	protected Driver driver;
 
-	public enum Label {
+	public enum Label
+	{
 		Document, Paragraph, Sentence, Token, Lemma, Pos
 	}
 
@@ -51,9 +60,127 @@ public class Neo4jQueryHandlerNew extends AbstractQueryHandler
 	}
 
 	@Override
-	public void storeJCasDocument(JCas document)
+	public void storeJCasDocument(final JCas document)
 	{
+		/* Document creation. */
+		final String documentId = DocumentMetaData.get(document)
+				.getDocumentId();
+		Session session = this.driver.session();
+		session.writeTransaction(tx -> {
+			String documentQuery = "MERGE (d:" + Label.Document + " {id:'" + documentId + "'}) SET d.text = '" + document.getDocumentText() + "', d.language = '" + document.getDocumentLanguage() + "'";
+			tx.run(documentQuery);
 
+			/*
+			 * Iterate over each element of the jCas that was annotated as a Pa-
+			 * ragraph. Each paragraph has properties for its beginning and en-
+			 * ding character position and the id of the document it is con-
+			 * tained in. This is done so identical paragraphs don't have rela-
+			 * tionships with more than one document.
+			 */
+			Paragraph previousParagraph = null;
+			for (Paragraph paragraph
+					: JCasUtil.select(document, Paragraph.class))
+			{
+				// Create paragraph (if not exists) and add relationship from
+				// document.
+				String paragraphQuery = "MATCH (d:" + Label.Document + " {id:'" + documentId + "'}) ";
+
+				// Add successor relationship from previous paragraph (if
+				// exists) to current paragraph.
+				// Match has to be done before the first Merge, so this is split
+				// into two parts.
+				if (previousParagraph != null)
+				{
+					paragraphQuery += "MATCH (p_prev:" + Label.Paragraph + " {id:'" + documentId + "', begin:'" + previousParagraph.getBegin() + "', end:'" + previousParagraph.getEnd() + "'}) ";
+				}
+
+				paragraphQuery += "MERGE (p:" + Label.Paragraph + " {id:'" + documentId + "', begin:'" + paragraph.getBegin() + "', end:'" + paragraph.getEnd() + "'}) "
+						+ "MERGE (d)-[:" + Relationship.DocumentHasParagraph + "]->(p)";
+
+				// Continue adding successor relationship.
+				if (previousParagraph != null)
+				{
+					paragraphQuery += "MERGE (p_prev)-[:" + Relationship.NextParagraph + "]->(p)";
+				}
+
+				tx.run(paragraphQuery);
+
+				previousParagraph = paragraph;
+
+
+				Sentence previousSentence = null;
+				for (Sentence sentence : JCasUtil.selectCovered(
+						document,
+						Sentence.class, paragraph
+				))
+				{
+					// Create sentence (if not exists) and add relationship from
+					// document and to paragraph.
+					String sentenceQuery = "MATCH (d:" + Label.Document + " {id:'" + documentId + "'}) "
+							+ "MATCH (p:" + Label.Paragraph + " {id:'" + documentId + "', begin:'" + paragraph.getBegin() + "', end:'" + paragraph.getEnd() + "'}) ";
+
+					// Add successor relationship from previous sentence (if
+					// exists) to current sentence.
+					// Match has to be done before the first Merge, so this is split
+					// into two parts.
+					if (previousSentence != null)
+					{
+						sentenceQuery += "MATCH (s_prev:" + Label.Sentence + " {id:'" + documentId + "', begin:'" + previousSentence.getBegin() + "', end:'" + previousSentence.getEnd() + "'}) ";
+					}
+
+					sentenceQuery += "MERGE (s:" + Label.Sentence + " {id:'" + documentId + "', begin:'" + sentence.getBegin() + "', end:'" + sentence.getEnd() + "'}) "
+							+ "MERGE (d)-[:" + Relationship.DocumentHasSentence + "]->(s) "
+							+ "MERGE (s)-[:" + Relationship.SentenceInParagraph + "]->(p)";
+
+					// Continue adding successor relationship.
+					if (previousSentence != null)
+					{
+						sentenceQuery += "MERGE (s_prev)-[:" + Relationship.NextSentence + "]->(s)";
+					}
+
+					tx.run(sentenceQuery);
+
+					previousSentence = sentence;
+
+
+					Token previousToken = null;
+					for (Token token : JCasUtil.selectCovered(document, Token.class, sentence))
+					{
+						// Create token (if not exists) and add relationship
+						// from Document and to Paragraph and to Sentence.
+						// Also create Lemma and Pos and add relationships from
+						// Token to them as well as from Document to Lemma.
+						String tokenQuery ="MATCH (d:" + Label.Document + " {id:'" + documentId + "'}) "
+								+ "MATCH (p:" + Label.Paragraph + " {id:'" + documentId + "', begin:'" + paragraph.getBegin() + "', end:'" + paragraph.getEnd() + "'}) "
+								+ "MATCH (s:" + Label.Sentence + " {id:'" + documentId + "', begin:'" + sentence.getBegin() + "', end:'" + sentence.getEnd() + "'}) ";
+
+						if (previousToken != null)
+						{
+							tokenQuery += "MATCH (t_prev:" + Label.Token + " {id:'" + documentId + "', begin:'" + previousToken.getBegin() + "', end:'" + previousToken.getEnd() + "', value='" + previousToken.getCoveredText() + "'}) ";
+						}
+
+						tokenQuery += "MERGE (t:" + Label.Token + " {id:'" + documentId + "', begin:'" + token.getBegin() + "', end:'" + token.getEnd() + "', value:'" + token.getCoveredText() + "'}) "
+								+ "MERGE (pos:" + Label.Pos + " {value:'" + token.getPos().getPosValue() + "'}) "
+								+ "MERGE (l:" + Label.Lemma + " {value:'" + token.getLemma().getValue() + "'}) "
+								+ "MERGE (d)-[:" + Relationship.DocumentHasToken + "]->(t) "
+								+ "MERGE (t)-[:" + Relationship.TokenInParagraph + "]->(p) "
+								+ "MERGE (t)-[:" + Relationship.TokenInSentence + "]->(s) "
+								+ "MERGE (t)-[:" + Relationship.TokenHasLemma + "]->(l) "
+								+ "MERGE (t)-[:" + Relationship.TokenAtPos + "]->(pos) "
+								+ "MERGE (d)-[:" + Relationship.DocumentHasLemma + "]->(l)";
+
+						if (previousToken != null) {
+							tokenQuery += "MERGE (t_prev)-[:" + Relationship.NextToken + "]->(t)";
+						}
+
+						tx.run(tokenQuery);
+
+						previousToken = token;
+					}
+				}
+			}
+			return 1;
+		});
 	}
 
 	@Override
