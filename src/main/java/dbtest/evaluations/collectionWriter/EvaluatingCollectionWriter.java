d9245ba3370c8fefa49ee926c6f77f508897aa42
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-04 18:42:51 +0100

    add abstract EvaluatingCollectionWriter

---
+++ src/main/java/dbtest/evaluations/collectionWriter/EvaluatingCollectionWriter.java
@@ -0,0 +1,8 @@
+package dbtest.evaluations.collectionWriter;
+
+import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+
+public abstract class EvaluatingCollectionWriter extends JCasConsumer_ImplBase
+{
+	public static final String PARAM_OUTPUT_FILE = "outputFile";
+}
