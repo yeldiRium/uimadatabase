2f176393dda851c46964993c4da66e14403d4d8c
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-05 12:18:57 +0100

    add uima configuration parameters to evaluating abstract classes

--- src/main/java/dbtest/evaluations/collectionReader/EvaluatingCollectionReader.java
+++ src/main/java/dbtest/evaluations/collectionReader/EvaluatingCollectionReader.java
@@ -1,12 +1,19 @@
 package dbtest.evaluations.collectionReader;
 
 import org.apache.uima.fit.component.CasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 public abstract class EvaluatingCollectionReader extends CasCollectionReader_ImplBase
 {
 	protected static final Logger logger =
 			Logger.getLogger(EvaluatingCollectionReader.class.getName());
+
+	// UIMA Parameters
 	public static final String PARAM_OUTPUT_FILE = "outputFile";
+
+	@ConfigurationParameter(name = PARAM_OUTPUT_FILE, mandatory = false)
+	public File outputFile;
 }
--- src/main/java/dbtest/evaluations/collectionWriter/EvaluatingCollectionWriter.java
+++ src/main/java/dbtest/evaluations/collectionWriter/EvaluatingCollectionWriter.java
@@ -1,12 +1,19 @@
 package dbtest.evaluations.collectionWriter;
 
 import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 public abstract class EvaluatingCollectionWriter extends JCasConsumer_ImplBase
 {
 	protected static final Logger logger =
 			Logger.getLogger(EvaluatingCollectionWriter.class.getName());
+
+	// UIMA Parameters
 	public static final String PARAM_OUTPUT_FILE = "outputFile";
+
+	@ConfigurationParameter(name = PARAM_OUTPUT_FILE, mandatory = false)
+	public File outputFile;
 }
