03366e87548eb3053c7cde816744b1766f4464f0
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-09 14:01:43 +0100

    move tfidf calculation methods into AbstractQueryHandler, since they exclusively rely on other interface methods

--- src/main/java/dbtest/queryHandler/AbstractQueryHandler.java
+++ src/main/java/dbtest/queryHandler/AbstractQueryHandler.java
@@ -1,5 +1,88 @@
 package dbtest.queryHandler;
 
+import dbtest.queryHandler.exceptions.DocumentNotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public abstract class AbstractQueryHandler implements QueryHandlerInterface
 {
+	@Override
+	public double calculateInverseDocumentFrequency(String lemma)
+	{
+		return Math.log((countElementsOfType(ElementType.Document) /
+				(double) countDocumentsContainingLemma(lemma))
+		);
+	}
+
+	@Override
+	public Map<String, Double>
+	calculateInverseDocumentFrequenciesForLemmataInDocument(String documentId)
+			throws DocumentNotFoundException
+	{
+		double docCount = (double) countElementsOfType(ElementType.Document);
+		Map<String, Double> inverseDocumentFrequencies =
+				new ConcurrentHashMap<>();
+		Set<String> lemmata = getLemmataForDocument(documentId);
+		Map<String, Integer> lemmaOccurenceCount =
+				this.countOccurencesForEachLemmaInAllDocuments();
+
+		lemmata.parallelStream().forEach(e -> {
+			inverseDocumentFrequencies.put(
+					e.replaceAll("\"", ""),
+					Math.log(docCount /
+							((double) lemmaOccurenceCount.getOrDefault(e, 0))
+					)
+			);
+		});
+		return inverseDocumentFrequencies;
+	}
+
+	@Override
+	public double calculateTFIDFForLemmaInDocument(
+			String lemma,
+			String documentId
+	) throws DocumentNotFoundException
+	{
+		return this.calculateTermFrequencyWithLogNormForLemmaInDocument(
+				lemma, documentId
+		) * this.calculateInverseDocumentFrequency(lemma);
+	}
+
+	@Override
+	public Map<String, Double> calculateTFIDFForLemmataInDocument(
+			String documentId
+	) throws DocumentNotFoundException
+	{
+		Map<String, Double> tf =
+				calculateTermFrequenciesForLemmataInDocument(documentId);
+		Map<String, Double> idf =
+				calculateInverseDocumentFrequenciesForLemmataInDocument(
+						documentId
+				);
+		Map<String, Double> tfidf = new ConcurrentHashMap<>();
+		tf.entrySet().parallelStream().forEach(
+				e -> tfidf.put(
+						e.getKey(),
+						e.getValue() * idf.get(e.getKey())
+				)
+		);
+		return tfidf;
+	}
+
+	@Override
+	public Map<String, Map<String, Double>>
+	calculateTFIDFForLemmataInAllDocuments()
+	{
+		HashMap<String, Map<String, Double>> tfidfs = new HashMap<>();
+		this.getDocumentIds().forEach(documentId -> {
+			tfidfs.put(
+					documentId,
+					this.calculateTFIDFForLemmataInDocument(documentId)
+			);
+		});
+		return tfidfs;
+	}
 }
