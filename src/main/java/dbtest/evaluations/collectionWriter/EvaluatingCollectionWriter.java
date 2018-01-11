82e7be31b8d64b26bfe500fd4743c103f6d8b38e
Hannes Leutloff <hannes.leutloff@aol.de>
2018-01-11 10:49:44 +0100

    make EvaluatingCollectionWriter non-abstract

--- src/main/java/dbtest/evaluations/collectionWriter/EvaluatingCollectionWriter.java
+++ src/main/java/dbtest/evaluations/collectionWriter/EvaluatingCollectionWriter.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
-public abstract class EvaluatingCollectionWriter extends JCasConsumer_ImplBase
+public class EvaluatingCollectionWriter extends JCasConsumer_ImplBase
 {
 	protected static final Logger logger =
 			Logger.getLogger(EvaluatingCollectionWriter.class.getName());
