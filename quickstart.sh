3b6a23a5fa6030c5d04d2b62cbb7f202d3cce068
Hannes Leutloff <hannes.leutloff@aol.de>
2018-02-01 20:05:10 +0100

    add quickstart script that compiles and runs the full pipeline

---
+++ quickstart.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+if [ ! -d ~/nlpdbeval_data ]
+then
+	mkdir ~/nlpdbeval_data
+fi
+
+if [ ! -d ~/nlpdbeval_data/input ]
+then
+	mkdir ~/nlpdbeval_data/input
+	echo "Please add some input files and set appropriate configuration values."
+fi
+
+if [ ! -d ~/nlpdbeval_data/output ]
+then
+	mkdir ~/nlpdbeval_data/output
+fi
+
+mvn clean package
+docker-compose build
+docker-compose run main java -cp /code/target.jar org.hucompute.services.uima.eval.main.EvaluationPipeline
+docker-compose run main java -cp /code/target.jar org.hucompute.services.uima.eval.main.VisualizationPipeline
+
+if [ -d ~/nlpdbeval_data/pdfs ]
+then
+	rm -r ~/nlpdbeval_data/pdfs
+fi
+mkdir ~/nlpdbeval_data/pdfs
+
+cd $(mktemp -d)
+for filename in ~/nlpdbeval_data/output/graphs/*.tex
+do
+	pdflatex -interaction batchmode $filename
+done
+
+mv *.pdf ~/nlpdbeval_data/pdfs
+rm *.log
+rm *.aux
+cd -
