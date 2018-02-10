a1f7425e325036332e920764a39102d8387ac93f
Hannes Leutloff <hannes.leutloff@aol.de>
2018-02-10 13:49:26 +0100

    update MySQLQueryHandler: implement setting up and clearing database

--- src/main/java/org/hucompute/services/uima/eval/database/abstraction/implementation/MySQLQueryHandler.java
+++ src/main/java/org/hucompute/services/uima/eval/database/abstraction/implementation/MySQLQueryHandler.java
@@ -12,6 +12,8 @@ import org.apache.uima.jcas.JCas;
 import org.hucompute.services.uima.eval.database.abstraction.exceptions.QHException;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -28,13 +30,128 @@ public class MySQLQueryHandler extends AbstractQueryHandler
 	@Override
 	public void setUpDatabase()
 	{
+		try (Statement aStatement = this.connection.createStatement())
+		{
+			String dropTables = "DROP TABLE IF EXISTS documentLemmaMap, " +
+					"tokenLemmaMap, " + ElementType.Pos + ", " +
+					ElementType.Lemma + ", " + ElementType.Token + ", " +
+					ElementType.Sentence + ", " + ElementType.Paragraph + ", " +
+					ElementType.Document + ";";
+			String createDocumentTable = "CREATE TABLE " + ElementType.Document + " ( " +
+					"  id VARCHAR(50) NOT NULL, " +
+					"  text MEDIUMTEXT, " +
+					"  language VARCHAR(50), " +
+					"  PRIMARY KEY (id) " +
+					")";
+			String createParagraphTable = "CREATE TABLE " + ElementType.Paragraph + " ( " +
+					"  id BIGINT NOT NULL AUTO_INCREMENT, " +
+					"  documentId VARCHAR(50) NOT NULL, " +
+					"  previousParagraphId BIGINT NOT NULL, " +
+					"  begin INT NOT NULL, " +
+					"  end INT NOT NULL, " +
+					"  PRIMARY KEY (id), " +
+					"  FOREIGN KEY (documentId) REFERENCES " + ElementType.Document + "(id), " +
+					"  FOREIGN KEY (previousParagraphId) REFERENCES " + ElementType.Paragraph + "(id) " +
+					")";
+			String createSentenceTable = "CREATE TABLE " + ElementType.Sentence + " ( " +
+					"  id BIGINT NOT NULL AUTO_INCREMENT, " +
+					"  paragraphId BIGINT NOT NULL, " +
+					"  documentId VARCHAR(50) NOT NULL, " +
+					"  previousSentenceId BIGINT NOT NULL, " +
+					"  begin INT NOT NULL, " +
+					"  end INT NOT NULL, " +
+					"  PRIMARY KEY (id), " +
+					"  FOREIGN KEY (paragraphId) REFERENCES " + ElementType.Paragraph + "(id), " +
+					"  FOREIGN KEY (documentId) REFERENCES " + ElementType.Document + "(id), " +
+					"  FOREIGN KEY (previousSentenceId) REFERENCES " + ElementType.Sentence + "(id) " +
+					")";
+			String createTokenTable = "CREATE TABLE " + ElementType.Token + " ( " +
+					"  id BIGINT NOT NULL AUTO_INCREMENT, " +
+					"  sentenceId BIGINT NOT NULL, " +
+					"  paragraphId BIGINT NOT NULL, " +
+					"  documentId VARCHAR(50) NOT NULL, " +
+					"  previousTokenId BIGINT NOT NULL, " +
+					"  value VARCHAR(255) NOT NULL, " +
+					"  begin INT NOT NULL, " +
+					"  end INT NOT NULL, " +
+					"  PRIMARY KEY (id), " +
+					"  FOREIGN KEY (sentenceId) REFERENCES " + ElementType.Sentence + "(id), " +
+					"  FOREIGN KEY (paragraphId) REFERENCES " + ElementType.Paragraph + "(id), " +
+					"  FOREIGN KEY (documentId) REFERENCES " + ElementType.Document + "(id), " +
+					"  FOREIGN KEY (previousTokenId) REFERENCES " + ElementType.Token + "(id) " +
+					")";
+			String createLemmaTable = "CREATE TABLE " + ElementType.Lemma + " ( " +
+					"  id BIGINT NOT NULL AUTO_INCREMENT, " +
+					"  value VARCHAR(255) NOT NULL, " +
+					"  PRIMARY KEY (id), " +
+					"  UNIQUE(value) " +
+					")";
+			String createPosTable = "CREATE TABLE " + ElementType.Pos + " ( " +
+					"  tokenId BIGINT NOT NULL, " +
+					"  begin INT NOT NULL, " +
+					"  end INT NOT NULL, " +
+					"  FOREIGN KEY (tokenId) REFERENCES " + ElementType.Token + "(id) " +
+					")";
 
+			String createTokenLemmaMap = "CREATE TABLE tokenLemmaMap ( " +
+					"  tokenId BIGINT NOT NULL, " +
+					"  lemmaId BIGINT NOT NULL, " +
+					"  FOREIGN KEY (tokenId) REFERENCES " + ElementType.Token + "(id), " +
+					"  FOREIGN KEY (lemmaId) REFERENCES " + ElementType.Lemma + "(id) " +
+					")";
+			String createDocumentLemmaMap = "CREATE TABLE documentLemmaMap ( " +
+					"  documentId VARCHAR(50) NOT NULL, " +
+					"  lemmaId BIGINT NOT NULL, " +
+					"  FOREIGN KEY (documentId) REFERENCES " + ElementType.Document + "(id), " +
+					"  FOREIGN KEY (lemmaId) REFERENCES " + ElementType.Lemma + "(id) " +
+					")";
+
+			aStatement.executeUpdate(dropTables);
+			aStatement.executeUpdate(createDocumentTable);
+			aStatement.executeUpdate(createParagraphTable);
+			aStatement.executeUpdate(createSentenceTable);
+			aStatement.executeUpdate(createTokenTable);
+			aStatement.executeUpdate(createLemmaTable);
+			aStatement.executeUpdate(createPosTable);
+			aStatement.executeUpdate(createTokenLemmaMap);
+			aStatement.executeUpdate(createDocumentLemmaMap);
+			} catch (SQLException e)
+		{
+			throw new QHException(e);
+		}
 	}
 
+	/**
+	 * Delete Tokens, Sentences and Paragraphs in reversed order to not run into
+	 * problems with the self-referencing foreign key.
+	 */
 	@Override
 	public void clearDatabase()
 	{
+		try (Statement aStatement = this.connection.createStatement())
+		{
+			String clearTokenLemmaMap = "DELETE FROM tokenLemmaMap;";
+			String clearDocumentLemmaMap = "DELETE FROM documentLemmaMap;";
 
+			String clearLemmata = "DELETE FROM " + ElementType.Lemma + ";";
+			String clearPos = "DELETE FROM " + ElementType.Pos + ";";
+			String clearTokens = "DELETE FROM " + ElementType.Token + " ORDER BY id DESC;";
+			String clearSentences = "DELETE FROM " + ElementType.Sentence + " ORDER BY id DESC;";
+			String clearParagraphs = "DELETE FROM " + ElementType.Paragraph + " ORDER BY id DESC;";
+			String clearDocuments = "DELETE FROM " + ElementType.Document + ";";
+
+			aStatement.executeUpdate(clearTokenLemmaMap);
+			aStatement.executeUpdate(clearDocumentLemmaMap);
+			aStatement.executeUpdate(clearLemmata);
+			aStatement.executeUpdate(clearPos);
+			aStatement.executeUpdate(clearTokens);
+			aStatement.executeUpdate(clearSentences);
+			aStatement.executeUpdate(clearParagraphs);
+			aStatement.executeUpdate(clearDocuments);
+		} catch (SQLException e)
+		{
+			throw new QHException(e);
+		}
 	}
 
 	@Override
