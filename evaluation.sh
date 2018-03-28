a4f08b9eb0d55584bb33bf8b394d827f6ab9c5b8
Hannes Leutloff <hannes.leutloff@aol.de>
2018-03-28 17:16:53 +0200

    fix typo in evaluation.sh

--- evaluation.sh
+++ evaluation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 java -jar target.jar evaluate -e write -d ArangoDB
-java -jar targat.jar evaluate -e read -d ArangoDB
-java -jar targat.jar evaluate -e query -d ArangoDB
-java -jar targat.jar evaluate -e calculate -d ArangoDB
-java -jar targat.jar evaluate -e complex-query -d ArangoDB
+java -jar target.jar evaluate -e read -d ArangoDB
+java -jar target.jar evaluate -e query -d ArangoDB
+java -jar target.jar evaluate -e calculate -d ArangoDB
+java -jar target.jar evaluate -e complex-query -d ArangoDB
 java -jar target.jar evaluate -e write -d Neo4j
-java -jar targat.jar evaluate -e read -d Neo4j
-java -jar targat.jar evaluate -e query -d Neo4j
-java -jar targat.jar evaluate -e calculate -d Neo4j
-java -jar targat.jar evaluate -e complex-query -d Neo4j
+java -jar target.jar evaluate -e read -d Neo4j
+java -jar target.jar evaluate -e query -d Neo4j
+java -jar target.jar evaluate -e calculate -d Neo4j
+java -jar target.jar evaluate -e complex-query -d Neo4j
 java -jar target.jar visualize
